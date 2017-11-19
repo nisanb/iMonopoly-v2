917130ee8106d53b2fdc3dabe509d9d06a9bc1fd
Nisan Bahar <nisan.univ@gmail.com>
2017-11-19 18:04:10 +0200

    Added more screens

--- src/Utils/Window.java
+++ src/Utils/Window.java
@@ -2,7 +2,10 @@ package Utils;
 
 public enum Window {
 	Login("login"), MainBoard("Board"), Login2("login2"),
-	Player_Menu("Player_Menu");
+	Player_Menu("Player_Menu"),
+	Player_Statistics("Player_Statistics"),
+	SASGOASJG("Player_Statistics"),
+	Num_Of_Players("NumOfPlayers");
 	String fxmlFile;
 	
 	Window(String fxmlFile){
--- src/View/NumOfPlayers.java
+++ src/View/NumOfPlayers.java
@@ -1,21 +1,17 @@
 /**
- * Sample Skeleton for 'Player_Menu.fxml' Controller Class
+ * Sample Skeleton for 'NumOfPlayers.fxml' Controller Class
  */
 
 package View;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
-import Controller.iWindow;
-import Utils.Window;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
-public class Player_Menu {
+public class NumOfPlayers {
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -26,16 +22,13 @@ public class Player_Menu {
     @FXML // fx:id="btnVolume"
     private ImageView btnVolume; // Value injected by FXMLLoader
 
-    @FXML // fx:id="lblUserLogged"
-    private Label lblUserLogged; // Value injected by FXMLLoader
-
     @FXML
     void MouseEntered(MouseEvent event) {
 
     }
 
     @FXML
-    void doLogout(ActionEvent event) {
+    void doLogin(ActionEvent event) {
 
     }
 
@@ -44,23 +37,13 @@ public class Player_Menu {
 
     }
     
-
-    @FXML
-    void OpenNumOfPlayers(ActionEvent event) {
-    	
-    	iWindow.swap(Window.Num_Of_Players);
-
-    }
-
-    @FXML
-    void openStatistics(ActionEvent event) {
-    	iWindow.swap(Window.SASGOASJG);
-    }
+    
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
-        assert btnVolume != null : "fx:id=\"btnVolume\" was not injected: check your FXML file 'Player_Menu.fxml'.";
-        assert lblUserLogged != null : "fx:id=\"lblUserLogged\" was not injected: check your FXML file 'Player_Menu.fxml'.";
+        assert btnVolume != null : "fx:id=\"btnVolume\" was not injected: check your FXML file 'NumOfPlayers.fxml'.";
+        
+        
 
     }
 }
