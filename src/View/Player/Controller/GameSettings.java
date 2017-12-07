f11ac8be06386a2cc8ce53ea83b5c04adb6f9ccd
Mickey Shalev <Mickey@gmail.com>
2017-12-07 18:02:34 +0200

    Game settings empty method remove and javadoc was added

--- src/View/Player/Controller/GameSettings.java
+++ src/View/Player/Controller/GameSettings.java
@@ -8,30 +8,16 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
-import java.util.jar.Attributes.Name;
 
 import Controller.iWindow;
-import Entity.Answer;
-import Entity.MonDB;
-import Entity.Question;
-import Entity.User;
 import Utils.Param;
-import Utils.QuestionStrength;
-import Utils.QuestionTag;
 import Utils.Window;
 import View.IManagement;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.Slider;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
@@ -41,15 +27,13 @@ import javafx.scene.input.SwipeEvent;
 
 public class GameSettings {
 
-	private IManagement _mng = iWindow.getManagement();
+	//========================================== FX COMPONENTS ================================================
 
 	@FXML // ResourceBundle that was given to the FXMLLoader
 	private ResourceBundle resources;
 
 	@FXML
 	private Label txt2Err;
-	
-
 
 	@FXML // URL location of the FXML file that was given to the FXMLLoader
 	private URL location;
@@ -98,73 +82,79 @@ public class GameSettings {
 
 	@FXML // fx:id="txt4"
 	private TextField txt4; // Value injected by FXMLLoader
+
+	@FXML // fx:id="txtNumOfRounds"
+	private Spinner<Integer> txtNumOfRounds; // Value injected by FXMLLoader
+
+	@FXML // fx:id="txtInitialSumOFMoney"
+	private Spinner<Integer> txtInitialSumOFMoney; // Value injected by FXMLLoader
+
+	@FXML // fx:id="txtBankrupt"
+	private Spinner<Integer> txtBankrupt; // Value injected by FXMLLoader
+
+	@FXML // fx:id="txtPaymentRelaseFromJail"
+	private Spinner<Integer> txtPaymentRelaseFromJail; // Value injected by FXMLLoader
+
+
+	//LOCAL VARIABLES
+	private IManagement _mng = iWindow.getManagement();
 	
-	   @FXML // fx:id="txtNumOfRounds"
-	    private Spinner<Integer> txtNumOfRounds; // Value injected by FXMLLoader
 
-	    @FXML // fx:id="txtInitialSumOFMoney"
-	    private Spinner<Integer> txtInitialSumOFMoney; // Value injected by FXMLLoader
+	// For generic checks
+	private List<TextField> txtfields = new ArrayList<>();
 
-	    @FXML // fx:id="txtBankrupt"
-	    private Spinner<Integer> txtBankrupt; // Value injected by FXMLLoader
-
-	    @FXML // fx:id="txtPaymentRelaseFromJail"
-	    private Spinner<Integer> txtPaymentRelaseFromJail; // Value injected by FXMLLoader
-	    
-
-
-	
-// back to previous form//
+	/**
+	 * Initialize fxml page
+	 */
 	@FXML
-	void back(ActionEvent event) {
-		iWindow.swap(Window.Player_Menu);
-	}
-	
-	
-
-	@FXML // This method is called by the FXMLLoader when initialization is
 	void initialize() {
-	/*	nickNamesValid.setVisible(false);*/
+		
+		//nickNamesValid.setVisible(false);
 		txtfields.add(txt2);
 		txtfields.add(txt3);
 		txtfields.add(txt4);
-		
 
-		
+		//reset visibility
 		txt3.setVisible(false);
 		label3.setVisible(false);
 		txt4.setVisible(false);
 		label4.setVisible(false);
-
 		img3.setVisible(false);
 		img4.setVisible(false);
 
-		/*txt2Err.setVisible(false);*/
-
+		//auto set logged in user
 		txt1.setText(_mng.getLoggedPlayer());
 		txt1.setDisable(true);
-		
+
+		//get game params
 		txtBankrupt.getValueFactory().setValue((Integer) _mng.getParam(Param.BANKRUPTCY));;
 		txtInitialSumOFMoney.getValueFactory().setValue((Integer) _mng.getParam(Param.STARTING_CASH));
 		txtNumOfRounds.getValueFactory().setValue((Integer) _mng.getParam(Param.MAX_ROUNDS));;
 		txtPaymentRelaseFromJail.getValueFactory().setValue((Integer) _mng.getParam(Param.RELEASE_FROM_JAIL));
-		
+
+		//reset errors
 		errorLabelControl(null, false);
 	}
-	
-	
-	
-	// For generic checks
-	private List<TextField> txtfields = new ArrayList<>();
-		
+
+	//====================================== ACTION EVENTS ====================================================
+
+	/**
+	 * This method send the user back to pervious form
+	 * @param event
+	 */
+	@FXML
+	void back(ActionEvent event) {
+		iWindow.swap(Window.Player_Menu);
+	}
+
+
 	@FXML
 	void check1(MouseEvent event) {
 
 		Integer res = Integer.valueOf((int) slide1.getValue());
-
 		txt2Err.setVisible(false);
-	/*	nickNamesValid.setVisible(false);*/
 		
+		//this condition gets how many users are going to participate in the game and get input accordingly
 		switch (res) {
 
 		case 2:
@@ -183,21 +173,21 @@ public class GameSettings {
 			txt3.setVisible(false);
 			label3.setVisible(false);
 			img3.setVisible(false);
-		
-			
+
+
 			txt4.setVisible(false);
 			label4.setVisible(false);
 			img4.setVisible(false);
-		
+
 			break;
 
 		case 3:
 			txt1.setVisible(true);
 			label1.setVisible(true);
 			img1.setVisible(true);
-			
+
 			txt2Err.setVisible(false);
-		
+
 			/*nickNamesValid.setVisible(false);*/
 			txt2.clear();
 			txt4.clear();
@@ -222,13 +212,13 @@ public class GameSettings {
 
 			label1.setVisible(true);
 			img1.setVisible(true);
-			
+
 			txt2Err.setVisible(false);
-		
+
 			txt3.clear();
 			txt4.clear();
 			txt2.clear();
-		
+
 			txt2.setVisible(true);
 			label2.setVisible(true);
 			img2.setVisible(true);
@@ -242,64 +232,21 @@ public class GameSettings {
 			img4.setVisible(true);
 			break;
 		}
-		
-	}
-
-	@FXML
-	void CheckRight(SwipeEvent event) {
 
 	}
 
-	@FXML
-	void check2(MouseEvent event) {
-
-	}
 
 	@FXML
 	void doSwapMusic(MouseEvent event) {
 
 	}
 
-	@FXML
-	void hidelabel1(MouseEvent event) {
 
-	}
 
-	@FXML
-	void hidelabel2(MouseEvent event) {
-
-	}
-
-	@FXML
-	void hidelabel3(MouseEvent event) {
-
-	}
-
-	@FXML
-	void hidelabel4(MouseEvent event) {
-
-	}
-
-	@FXML
-	void hidetextfield1(ActionEvent event) {
-
-	}
-
-	@FXML
-	void hidetextfield2(ActionEvent event) {
-
-	}
-
-	@FXML
-	void hidetextfield3(ActionEvent event) {
-
-	}
-
-	@FXML
-	void hidetextfield4(ActionEvent event) {
-
-	}
-
+	/**
+	 * This methods opens the game boared with user's input
+	 * @param event
+	 */
 	@FXML
 	void openBoard(ActionEvent event) {
 
@@ -322,29 +269,34 @@ public class GameSettings {
 
 		if (playerList.size() != res) {
 			// There is a problem//
-		
+
 			errorLabelControl("You must fill  the empty fields ", true);
-			
+
 		} 
 		else {
-			
+
 			if(duplicates(playerList))
 			{
 				errorLabelControl("Unable to register with the same nickname", true);
-				
+
 			}
 			else{
-			_mng.build(playerList);
-			iWindow.swap(Window.Game_UI);
+				_mng.build(playerList);
+				iWindow.swap(Window.Game_UI);
 			}
 		}
 	}
 
 
-    
-	
-	//========================================methods=======================================================//
-	
+
+
+	//============================================== METHODS =================================================//
+
+	/**
+	 * This helper method prevent from user to set more than one player with the same name
+	 * @param playerList
+	 * @return
+	 */
 	private boolean duplicates(final List<String> playerList)
 	{		
 		Set<String> dup=new HashSet<String>();
@@ -355,25 +307,30 @@ public class GameSettings {
 		}
 		return false;
 	}
-	
-    private void errorLabelControl(String msg, boolean visiblity) {
-    	this.txt2Err.setVisible(visiblity);
-    	if (msg == null) {
-    		this.txt2Err.setText(" ");
-    	}
-    	else {
-    		this.txt2Err.setText("Error: " + msg);
-    		System.out.println(msg);
-    	}
-    }
-	
-	
-	//============================================== CSS ==========================================================
-	
-    private void settxtBGC(TextField tf, String color) {
-    	tf.setStyle("-fx-background-color:" + color +";");
-    }
-	
-	
+
+	/**
+	 * This method controls the error message label
+	 * @param msg to present to user
+	 * @param visiblity of the label
+	 */
+	private void errorLabelControl(String msg, boolean visiblity) {
+		this.txt2Err.setVisible(visiblity);
+		if (msg == null) {
+			this.txt2Err.setText(" ");
+		}
+		else {
+			this.txt2Err.setText("Error: " + msg);
+			System.out.println(msg);
+		}
+	}
+
+
+	//============================================== CSS ======================================================
+
+	private void settxtBGC(TextField tf, String color) {
+		tf.setStyle("-fx-background-color:" + color +";");
+	}
+
+
 
 }
