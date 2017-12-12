9d7d0364ed10b639ab7934abd8c8bd43c983ba53
Nisan Bahar <nisan.univ@gmail.com>
2017-12-12 19:41:48 +0200

    Returned randomized dice roll

--- src/Controller/GameEngine.java
+++ src/Controller/GameEngine.java
@@ -228,7 +228,6 @@ public class GameEngine implements IGameEngine {
 		}
 
 		Integer moveToTile = (dice.getSum() + currentPlayer().getCurrentTile().getTileNumber()) % 40;
-		moveToTile = 5;
 		ui.movePlayer(currentPlayer().getNickName(), currentPlayer().getCurrentTile().getTileNumber(), moveToTile);
 		ui.allowFinishTurn(true);
 	}
