41592c5ea8332f3a4bbb43bc814d61fe6a84407c
Mickey Shalev <Mickey@gmail.com>
2017-12-31 20:23:47 +0200

    param unit test was fixed

--- JUnit/UT_Params.java
+++ JUnit/UT_Params.java
@@ -15,16 +15,15 @@ public class UT_Params {
 		//Try to initiate DB
 		Logger.initializeMyFileWriter();
 		MonDB db = MonDB.getInstance();
+		System.out.println(db.getParam(Param.STARTING_CASH));
 		
-		assertEquals(500000, db.getParam(Param.STARTING_CASH));
-		
-		db.setParam(Param.STARTING_CASH, 200000);
-		
-		assertEquals(200000, db.getParam(Param.STARTING_CASH));
+		//change param and check if it changed in db
+		db.setParam(Param.STARTING_CASH, 200000.0);
+		assertEquals(200000.0, db.getParam(Param.STARTING_CASH));
 	
+		//reset params and check if it changed in db
 		db.resetParamsToDefault();
-		
-		assertEquals(500000, db.getParam(Param.STARTING_CASH));
+		assertEquals(500000.0, db.getParam(Param.STARTING_CASH));
 	}
 
 }
